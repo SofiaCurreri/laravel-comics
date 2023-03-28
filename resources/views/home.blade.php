a00d2255c262ee02670c89870933ddeee0e1a0ca
SofiaCurreri <sofiacurreri@gmail.com>
2023-03-28 16:21:28 +0200

    comics array + imgs

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel 9 + Bootstrap Template</title>
+  <title>Laravel Comics</title>
 
   {{-- Includiamo gli assets con la direttiva @vite --}}
   @vite('resources/js/app.js')
@@ -14,11 +14,8 @@
 <body>
   <main>
     <div class="container d-flex flex-column jumbotron align-items-center justify-content-center text-center">
-      <img class="my-1" src="{{ Vite::asset('resources/img/logo.png') }}" alt="">
-      <h1 class="my-1">
-        Laravel 9
-        <span class="fs-6 fw-light text-muted"> - Bootstrap Template</span>
-      </h1>
+      {{-- <img class="my-1" src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> --}}
+      
 
     </div>
   </main>
